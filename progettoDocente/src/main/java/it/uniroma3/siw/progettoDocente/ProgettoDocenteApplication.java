c3bd96999ec9018a30bcff4001ffd619ddee0565
StormyT95 <63358109+StormyT95@users.noreply.github.com>
2021-05-30 16:14:20 +0200

    Add files via upload

---
+++ progettoDocente/src/main/java/it/uniroma3/siw/progettoDocente/ProgettoDocenteApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progettoDocente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoDocenteApplication.class, args);
+	}
+
+}
---
+++ progettoDocente/src/test/java/it/uniroma3/siw/progettoDocente/ProgettoDocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progettoDocente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoDocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
