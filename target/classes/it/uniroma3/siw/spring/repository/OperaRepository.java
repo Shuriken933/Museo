6bdfec3aa704290c8c2c7cb7e9210b6e0158d7bb
Eros <46653299+Shuriken933@users.noreply.github.com>
2021-06-14 23:54:59 +0200

    Add files via upload

---
+++ target/classes/it/uniroma3/siw/spring/repository/OperaRepository.java
@@ -0,0 +1,26 @@
+package it.uniroma3.siw.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.uniroma3.siw.spring.model.Artista;
+import it.uniroma3.siw.spring.model.Collezione;
+import it.uniroma3.siw.spring.model.Opera;
+
+public interface OperaRepository extends CrudRepository<Opera, Long>{
+	
+	/*public List<Opera> deleteOpera(String titolo);*/
+	
+	/*public List<Opera> deleteAllOpere(Collezione collezione);*/
+	
+	public List<Opera> findByArtista(Artista artista);
+	
+	
+	/*public boolean rimuoviTutteOpereDiUnArtista(Long idArtista);*/
+	
+	/*public List<Opera> findByAnno(Integer annoDiRealizzazione);*/
+	
+	public List<Opera> findByTitolo(String titolo);
+
+}
